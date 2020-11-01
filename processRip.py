e189770fdbb4eeef951897a3b0ee4b42681216ae
Bondnuy007 <bondnuy007@me.com>
2020-11-02 02:28:38 +0700

    V2

--- processRip.py
+++ processRip.py
@@ -9,7 +9,7 @@ import setup
 def start(profile):
     node_name = Profile_All(profile)
     if node_name is None:
-        raise Exception('Profile is Emty.')
+        raise Exception('Profile is not found.')
     else :
         router_name =  RouterName(node_name)
         router_name.detail()
